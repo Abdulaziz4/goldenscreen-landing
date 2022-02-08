e328e6f9201549348ed7b856b9b60e581a84dfbb
Abdulaziz Alqahtani <h-xa2012@hotmail.com>
2022-02-09 00:43:35 +0300

    Adding platforms badges

--- pages/landing.js
+++ pages/landing.js
@@ -11,30 +11,102 @@ export default function Landing() {
         <AppInfo>
           <h1>
             <span>صيانة جوالك&nbsp;</span>
-            <span>في مكانك</span>
+            <BoldSpan>في مكانك</BoldSpan>
           </h1>
+          <AppDescription>
+            خدمة صيانة جوالات متنقلة سريعة بتركيب <br />
+            مجاني بأيدي احترافية وين ماكنت في الرياض
+          </AppDescription>
+          <PlatformsBadge>
+            <DownloadImage>
+              <Image src="/assets/appstore-badge.svg" alt="" layout="fill" />
+            </DownloadImage>
+            <DownloadImage>
+              <Image src="/assets/googleplay-badge.svg" alt="" layout="fill" />
+            </DownloadImage>
+          </PlatformsBadge>
         </AppInfo>
-        <Image
-          src="/assets/repair-phone.png"
-          alt="repair"
-          height={300}
-          width={450}
-        />
+        <HeaderIllustration>
+          <Image src="/assets/repair-phone.png" alt="repair" layout="fill" />
+        </HeaderIllustration>
       </AppPromotion>
     </>
   );
 }
 
 const AppPromotion = styled.div`
-  height: 70vh;
+  height: 60vh;
   background-color: var(--yellow-color);
   display: flex;
+  width: 100vw;
+  justify-content: space-around;
+  /* align-items: center; */
+  padding-top: 40px;
+  /* font-family: "Cairo" !important; */
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    height: 100vh;
+    padding-top: 0px;
+  }
 `;
 
 const AppInfo = styled.div`
   color: --text-color;
+  margin: 20px 40px;
+  span {
+    font-weight: 300;
+    font-size: 43px;
+    /* font-size: 3.5vw; */
+    color: var(--text-color);
+  }
 
-  h1::nth-child(2) {
-    font-weight: 900;
+  @media only screen and (max-width: 768px) {
+    margin: 20px;
+    span {
+      font-size: 25px;
+    }
+  }
+`;
+
+const BoldSpan = styled.span`
+  font-weight: bolder !important;
+`;
+
+const AppDescription = styled.span`
+  font-size: 22px;
+  font-weight: lighter !important;
+`;
+
+const PlatformsBadge = styled.div`
+  margin: 20px 40px;
+  gap: 20px;
+  display: flex;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    gap: 5px;
+  }
+`;
+
+const DownloadImage = styled.div`
+  height: 130px;
+  width: 250px;
+  background-color: red;
+  position: relative;
+  @media only screen and (max-width: 768px) {
+    height: 90px;
+  }
+`;
+
+const HeaderIllustration = styled.div`
+  height: 350px;
+  width: 530px;
+  position: relative;
+  background-color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+    /* margin: 20px; */
   }
 `;
--- styles/globals.js
+++ styles/globals.js
@@ -1,17 +1,32 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
+// import cairo200 from "../public/assets/fonts/cairo-v17-arabic-200.woff";
+// import cairo200w2 from "../public/assets/fonts/cairo-v17-arabic-200.woff2";
+// import cairo300 from "../public/assets/fonts/cairo-v17-arabic-300.woff";
+// import cairo300w2 from "../public/assets/fonts/cairo-v17-arabic-300.woff2";
+// import cairo500 from "../public/assets/fonts/cairo-v17-arabic-500.woff";
+// import cairo500w2 from "../public/assets/fonts/cairo-v17-arabic-500.woff2";
+// import cairo600 from "../public/assets/fonts/cairo-v17-arabic-600.woff";
+// import cairo600w2 from "../public/assets/fonts/cairo-v17-arabic-600.woff2";
+// import cairo700 from "../public/assets/fonts/cairo-v17-arabic-700.woff";
+// import cairo700w2 from "../public/assets/fonts/cairo-v17-arabic-700.woff2";
+// import cairo800 from "../public/assets/fonts/cairo-v17-arabic-800.woff";
+// import cairo800w2 from "../public/assets/fonts/cairo-v17-arabic-800.woff2";
+// import cairo900 from "../public/assets/fonts/cairo-v17-arabic-900.woff";
+// import cairo900w2 from "../public/assets/fonts/cairo-v17-arabic-900.woff2";
+// import cairoReg from "../public/assets/fonts/cairo-v17-arabic-regular.woff";
+// import cairoRegw2 from "../public/assets/fonts/cairo-v17-arabic-regular.woff2";
 
+const GlobalStyle = createGlobalStyle`
 
 html,
 body {
   padding: 0;
   margin: 0;
-  font-family:  sans-serif;
+  font-family: Helvetica, sans-serif;
   --yellow-color: #FFEBAF;
   --background-color: #FFFEFB;
---text-color: #3E3E3E;
-
+  --text-color: #3E3E3E;
   background-color: #FFFEFB;
   direction: rtl;
 }
